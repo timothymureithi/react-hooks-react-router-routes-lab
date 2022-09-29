5761b4ea3cc4971afee7d69241ec65d273f03fb1
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-09-29 07:50:04 +0300

    completed lab

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,24 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
-}
+
+const displayActor = actors.map((actor) => (
+  <div key={actor.name}>
+    <h2>Name: {actor.name}</h2>
+    Movies:
+    <ul>
+      {actor.movies.map((movie) => (
+        <li key = {movie}>{movie}</li>
+      ))}
+    </ul>
+  </div>
+));
+return (
+  <div>
+    <h1>Actors Page</h1>
+  {displayActor}
+  </div>
+  
+)}
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,8 +1,27 @@
 import React from "react";
 import { directors } from "../data";
 
+/**This component should render the text Directors Page in an <h1>,
+ *  and make a new <div> for each director. The <div> should
+ *  contain the director's name and a <ul> with a list of their movies. */
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
-}
+const displayDirector = directors.map((director) => (
+  <div key={director.name}>
+    <h2>Name: {director.name}</h2>
+    Movies:
+    <ul>
+      {director.movies.map((movie) => (
+        <li key={movie}>{movie}</li>
+      ))}
+    </ul>
+  </div>
+));
+return (
+  <div>
+    <h1>Directors Page</h1>
+  {displayDirector}
+  </div>
+);
 
+}
 export default Directors;
